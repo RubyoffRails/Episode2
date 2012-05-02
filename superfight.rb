e90db2516e215e5a231df152df6ed01292d2bacc
Peter Aronoff <telemachus@arpinum.org>
2012-05-02 14:16:07 -0400

    Show each turn

--- superfight.rb
+++ superfight.rb
@@ -1,11 +1,15 @@
 Dir["./lib/*.rb"].each {|file| require file }
 
 
-puts "What is your first fighter's name?"
-fighter_a = $stdin.gets
-puts "What is your second fighter's name?"
-fighter_b = $stdin.gets
+print "What is your first fighter's name? "
+fighter_a = gets.chomp
+print "What is your second fighter's name? "
+fighter_b = gets.chomp
 
 match = Match.new(Fighter.new(fighter_a), Fighter.new(fighter_b))
+
+match.turns.each do |turn|
+  puts "#{fighter_a}'s strike: #{turn.move_a.ranking} :: #{fighter_b}'s strike #{turn.move_b.ranking}"
+end
 
 puts "The winner of match is ....... #{match.winner.name}"
