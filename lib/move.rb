3c2106dd809a86b5be321ea725c38fcbed1bae60
Peter Aronoff <telemachus@arpinum.org>
2012-05-03 08:44:29 -0400

    A strike is more than a block

--- lib/move.rb
+++ lib/move.rb
@@ -1,26 +1,25 @@
 class Move
   include Comparable
 
-	attr_reader :type, :ranking
-	def initialize(type)
-		@type = type
-        @ranking = create_ranking
-	end
+  attr_reader :type, :ranking
+  def initialize(type)
+    @type = type
+    @ranking = create_ranking
+  end
 
-    def <=>(other)
-      if self.type == :block && other.type == :block
-        0
-      else
-        self.ranking <=> other.ranking
-      end
+  def <=>(other)
+    if self.type == :block && other.type == :block
+      0
+    else
+      self.ranking <=> other.ranking
     end
+  end
 
-    private
-    # To make the strike versus block ranking a bit weighted, I've
-    # changed this. A strike is a random number 0-99, but a block is
-    # only 0-75. A little tilt in the strike's favor.
-    def create_ranking
-      @type == :strike ? rand(100) : rand(76)
-    end
-
+  private
+  # To make the strike versus block ranking a bit weighted, I've
+  # changed this. A strike is a random number 1-100, but a block is
+  # only 1-70. A little tilt in the strike's favor.
+  def create_ranking
+    @type == :strike ? rand(100).ceil : rand(70).ceil
+  end
 end
