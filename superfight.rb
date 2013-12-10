54ff9ac5a81dbc7435f64e553ae9fbc6508cb538
Mike Adeleke <madeleke13@gmail.com>
2013-12-09 22:00:41 -0600

    A lot of questions. need to go over it again

--- superfight.rb
+++ superfight.rb
@@ -6,6 +6,15 @@ fighter_a = $stdin.gets
 puts "What is your second fighter's name?"
 fighter_b = $stdin.gets
 
-match = Match.new(Fighter.new(fighter_a), Fighter.new(fighter_b))
+match = Match.new(Fighter.new(fighter_a), Fighter.new(fighter_b)) do
+	13.times.map do
+	  match = Match.new
+	    if @fighter_a.strike == turn.winner
+	        puts "Fighter A  -- #{@fighter_a.name} -- won"
+	    else
+	        puts "Fighter B  -- #{@fighter_b.name} -- won"
+	    end
+	end
+end
 
 puts "The winner of match is ....... #{match.winner.name}"
