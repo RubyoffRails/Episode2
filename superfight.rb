d51e80c29bc4d68cdc98f179990dc4e18da361af
Mike Adeleke <madeleke13@gmail.com>
2013-12-11 14:39:45 -0600

    More changes

--- superfight.rb
+++ superfight.rb
@@ -18,3 +18,34 @@ match = Match.new(Fighter.new(@fighter_a), Fighter.new(@fighter_b)).tap do
 end
 
 puts "The winner of match is ....... #{match.winner.name}"
+
+# Moving away from random winners
+
+class Move
+
+	attr_reader :strike, :block, :leg_sweep
+
+	def initialize
+		@strike = strike
+		@block = block
+		@leg_sweep = leg_sweep
+	end
+
+	def moves
+		strike = 50
+		block = 40
+		leg_sweep = 60
+	end
+end
+
+match = Match.new(Fighter.new(@fighter_a), Fighter.new(@fighter_b)) do
+	3.times.map do
+		match = Match.new
+	    if @fighter_a.moves > @fighter_b.moves
+	        puts "Fighter A  -- #{@fighter_a.name} -- won"
+	    else
+	        puts "Fighter B  -- #{@fighter_b.name} -- won"
+	    end
+	end
+end
+
